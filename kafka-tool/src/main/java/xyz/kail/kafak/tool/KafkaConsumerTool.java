3b24d9dd0498257894126776a4067f4eb8f2ed9e
kaibin.yang <kaibin.yang@ttpai.cn>
2019-08-10 18:14:15 +0800

    Kafka Tool

---
+++ kafka-tool/src/main/java/xyz/kail/kafak/tool/KafkaConsumerTool.java
@@ -0,0 +1,77 @@
+package xyz.kail.kafak.tool;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * 消费者 工具
+ */
+@Slf4j
+public class KafkaConsumerTool {
+
+    private static KafkaConsumer<String, String> consumer;
+
+    /**
+     * 消费者默认配置
+     */
+    public static synchronized KafkaConsumer<String, String> defaultConsumer(String servers, String groupId) {
+        if (null == consumer) {
+            consumer = new KafkaConsumer<>(defaultConf(servers, groupId));
+        }
+        return consumer;
+    }
+
+
+    /**
+     * 消费者默认配置
+     */
+    public static Map<String, Object> defaultConf(String servers, String groupId) {
+
+        final Map<String, Object> props = new HashMap<>();
+        // 连接地址
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
+        // GroupID
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        // 是否自动提交
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        // 自动提交的频率
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 100);
+        // Session超时设置
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 15000);
+        // 键的反序列化方式
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        // 值的反序列化方式
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        return props;
+    }
+
+
+    /**
+     * 从配置文件读取 消费者配置
+     */
+    public static synchronized KafkaConsumer<String, String> propertiesConsumer(String configFile) {
+        if (null != consumer) {
+            return consumer;
+        }
+
+        try (final InputStream inStream = ClassLoader.getSystemResourceAsStream(configFile)) {
+            final Properties properties = new Properties();
+            properties.load(inStream);
+            consumer = new KafkaConsumer<>(properties);
+        } catch (IOException ex) {
+            log.error("读取配置文件 {} 出错", configFile, ex);
+        }
+
+        return consumer;
+    }
+
+}
---
+++ kafka-tool/src/main/java/xyz/kail/kafak/tool/KafkaProducerTool.java
@@ -0,0 +1,78 @@
+package xyz.kail.kafak.tool;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * 生产者工具
+ */
+@Slf4j
+public class KafkaProducerTool {
+
+
+    private static KafkaProducer<String, String> producer;
+
+    /**
+     * 生产者默认配置
+     */
+    public static synchronized KafkaProducer<String, String> defaultProducer(String servers) {
+        if (null == producer) {
+            producer = new KafkaProducer<>(defaultConf(servers));
+        }
+        return producer;
+    }
+
+
+    /**
+     * 生产者默认配置
+     */
+    public static Map<String, Object> defaultConf(String servers) {
+
+        final Map<String, Object> conf = new HashMap<>();
+        // 连接地址
+        conf.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
+        // 重试，0为不启用重试机制
+        conf.put(ProducerConfig.RETRIES_CONFIG, 3);
+        // 控制批处理大小，单位为字节
+        conf.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        // 批量发送，延迟为1毫秒，启用该功能能有效减少生产者发送消息次数，从而提高并发量
+        conf.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        // 生产者可以使用的总内存字节来缓冲等待发送到服务器的记录
+        conf.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 1024000);
+        // 键的序列化方式
+        conf.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        // 值的序列化方式
+        conf.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return conf;
+    }
+
+
+    /**
+     * 生产者默认配置
+     */
+    public static synchronized KafkaProducer<String, String> propertiesProducer(String configFile) {
+        if (null != producer) {
+            return producer;
+        }
+
+        try (final InputStream inStream = ClassLoader.getSystemResourceAsStream(configFile)) {
+            final Properties properties = new Properties();
+            properties.load(inStream);
+            producer = new KafkaProducer<>(properties);
+        } catch (IOException ex) {
+            log.error("读取配置文件 {} 出错", configFile, ex);
+        }
+
+        return producer;
+    }
+
+}
